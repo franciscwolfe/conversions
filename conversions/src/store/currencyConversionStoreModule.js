874794b66fdbd92ad55e84e08a7973f15593dc7c
francis.wolfe@gmail.com <francis.wolfe@gmail.com>
2020-10-07 10:22:02 +0100

    fixed port number

--- conversions/src/store/currencyConversionStoreModule.js
+++ conversions/src/store/currencyConversionStoreModule.js
@@ -4,7 +4,7 @@ import Axios from 'axios'
 export default class CurrencyConversionStoreModule extends ConversionStoreModule {
     actions = {
         loadTypes(state, modulePath) {
-          Axios.get(`https://localhost:54112/currency`).then(
+          Axios.get(`https://localhost:44387/currency`).then(
             response => {
               response.data.unshift('');
               this.commit(`${modulePath}/updateTypes`, response.data)
@@ -18,7 +18,7 @@ export default class CurrencyConversionStoreModule extends ConversionStoreModule
           var searchOutputType = state.outputType;
           var searchInputAmount = state.inputAmount;
     
-          Axios.get(`https://localhost:54112/currency/Convert?inputCurrency=${searchInputType}&outputCurrency=${searchOutputType}&value=${searchInputAmount}`).then(
+          Axios.get(`https://localhost:44387/currency/Convert?inputCurrency=${searchInputType}&outputCurrency=${searchOutputType}&value=${searchInputAmount}`).then(
             response => {
               this.commit(`${modulePath}/updateResult`, 
               `${searchInputType}${'\xa0'}${Number(searchInputAmount).toFixed(2)} = ${searchOutputType}${'\xa0'}${response.data}`,
